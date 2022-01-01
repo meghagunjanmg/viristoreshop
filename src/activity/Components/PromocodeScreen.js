b9f197f2adafcc9c99bec83d87df8fe04c94a186
Gunjan@31 <meghagunjanmg@gmail.com>
2022-01-01 22:36:54 +0530

    first commit

---
+++ src/activity/Components/PromocodeScreen.js
@@ -0,0 +1,131 @@
+import React, {useState, useEffect} from "react";
+import {StyleSheet, 
+  View,
+  Text, 
+  SafeAreaView, 
+  ScrollView, 
+  TextInput,
+  Image, 
+  TouchableOpacity,
+  ActivityIndicator,
+  Alert
+} from 'react-native';
+import { EvilIcons, FontAwesome } from '@expo/vector-icons'; 
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+import * as Location from 'expo-location';
+import AsyncStorage from '@react-native-community/async-storage';
+import {connect} from "react-redux";
+import {getCouponDiscount} from "../../actions/itemsAction.js";
+import { MaterialIcons } from '@expo/vector-icons';
+import Loader from "./Loader.js";
+import Storage from "../Storage.js";
+
+
+const PromocodeScreen = (props) => {
+    var [promocodeData,setPromocodeData] = useState([]);
+    var cartID = "SZFA4895";
+    var [loading,setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        var formdata = new FormData();
+        formdata.append("cart_id", cartID);
+
+        var requestOptions = {
+          method: 'POST',
+          body: formdata,
+          redirect: 'follow'
+        };
+
+        fetch("http://shanya.myviristore.com/admin/api/couponlist", requestOptions)
+          .then(response => response.json())
+          .then(async result => {
+            if(result.status!=0)
+            {  console.log(result);
+              await setPromocodeData(result.data);
+              setLoading(false);
+            }
+            else{
+              setLoading(false);
+              Alert.alert(
+                "Sorry",
+                "No promo code available",
+                [
+                  {
+                    text: "OK",
+                    onPress: () =>  props.navigation.navigate("Cart")
+
+                  },
+                ]
+              );
+            }
+            })
+          .catch(error => {
+              console.log('error', error)
+              setLoading(false);
+        });
+    }, [])
+
+    const handleSubmitCupon = (coupon_code) =>{
+        setLoading(true);
+        console.log("Called")
+        var formdata = new FormData();
+        formdata.append("coupon_code", coupon_code);
+        formdata.append("cart_id", cartID);
+
+        var requestOptions = {
+          method: 'POST',
+          body: formdata,
+          redirect: 'follow'
+        };
+
+        fetch("http://shanya.myviristore.com/admin/api/apply_coupon", requestOptions)
+          .then(response => response.json())
+          .then(async result => {
+              console.log(result.data.coupon_discount)
+              var data = {discount:result.data.coupon_discount,couponName:coupon_code,couponApplied:true};
+                console.log("the final data is:- "+JSON.stringify(data))
+                await props.getCouponDiscount({discount:result.data.coupon_discount,couponName:coupon_code,couponApplied:true})
+                props.navigation.navigate("CartScreen");
+                setLoading(false);
+            })
+          .catch(error =>{ 
+            console.log('error', error)
+            setLoading(false);
+            });
+    }
+    return(
+        <ScrollView>
+            <Loader loading={loading}/>
+            {promocodeData.map(code => {
+                return(
+                <View
+                 key={code.coupon_id}
+                 style={{flexDirection:"row",width:"95%",marginLeft:"auto",marginRight:"auto",marginTop:10,marginBottom:5,backgroundColor:"white",padding: 10,elevation:10,borderRadius:10}}>
+                    <View style={{flex:1}}>
+                        <Text style={{color:"black",fontSize:20,fontWeight:"bold",marginBottom:10}}>{code.coupon_name}</Text>
+                        <Text style={{color:"grey"}}>{code.coupon_description}</Text>
+                    </View>
+                    <View style={{flex:1}}>
+                        <Text style={{borderWidth:1,borderColor:"grey",borderRadius:10,padding:5,textAlign:"center",marginBottom:10}}>{code.coupon_code}</Text>
+                        <TouchableOpacity 
+                            onPress={() => handleSubmitCupon(code.coupon_code)}
+                            style={{padding:7,backgroundColor:"orange",borderRadius:10}}>
+                            <Text style={{textAlign:"center",color:"white"}}>Apply</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>)
+            })}
+        </ScrollView>
+    )
+}
+const mapStateToProps = (state) => {
+    // console.log("State Contains:-"+ state)
+    // console.log(`Map State to props:- ${state.item.homepageData.status}`)
+    return({
+        //Here State.post is 
+        //Coming From -> "./reducers/index.js"
+        //where "post" is defined under combineReducers
+        item:state.item
+    })
+  }
+  export default connect(mapStateToProps,{getCouponDiscount})(PromocodeScreen);
---
+++ src/activity/ForgetPass.js
@@ -0,0 +1,135 @@
+import React, {useState} from "react";
+import { StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  SafeAreaView, ScrollView,
+  TouchableOpacity } from "react-native";
+import { SimpleLineIcons, Entypo, FontAwesome, MaterialIcons} from '@expo/vector-icons'; 
+
+const ForgetPass = ({ navigation }) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+                <TouchableOpacity onPress={() => navigation.navigate("LoginScreen")} style={styles.skip}>
+                    <Text style={styles.skipText}>&#8592; Back</Text> 
+                </TouchableOpacity>
+
+                <View style={styles.imageview}>
+                    <Image style={styles.image}
+                        source={require('../../assets/ic_launcher.png')}
+                        resizeMode="cover" />
+                </View>
+
+                <View>
+                    <Text style={styles.cred}>Forget Password</Text>
+                </View>
+
+                <View style={styles.searchSection}>
+                    <SimpleLineIcons style={styles.keyIcon} name="screen-smartphone" size={20} color="#238A02" />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Mobile Number"
+                        underlineColorAndroid="transparent"
+                        placeholderTextColor="#6C6969"
+                        onChangeText={(email) => setEmail(email)}
+                    />
+                </View>
+
+                <TouchableOpacity style={styles.loginBtn}>
+                    <Text style={styles.loginText}>VERIFY</Text>
+                </TouchableOpacity>
+
+        </ScrollView>
+    </SafeAreaView>
+
+    
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+   },
+   scrollView: {
+    backgroundColor: '#fff',
+    width: "100%",
+   },
+   imageview: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+   },
+   image :{
+    marginBottom: 20,
+    marginTop: 40,
+    marginLeft: "35%",
+    width: 100,
+    height:100,
+  },
+  cred: {
+    alignItems: "center",
+    fontSize: 25,
+    marginBottom: 20,
+    marginLeft: "20%",
+  },
+  searchSection: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderColor: "#6C6969",
+    borderWidth: 1,
+    borderRadius: 20,
+    width: "80%",
+    marginTop: 20,
+    marginLeft: "10%",
+  },
+  input: {
+    flex: 1,
+    paddingTop: 7,
+    paddingRight: 7,
+    paddingBottom: 7,
+    paddingLeft: 0,
+    color: '#424242',
+  },
+  keyIcon:{
+    padding: 7,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    color: '#FFFFFF',
+    backgroundColor: "#238A02",
+    marginTop: 30,
+    marginLeft: "10%",
+  },
+  loginText:{
+    color: "#FFFFFF",
+  },
+  skip:{
+    alignItems: "flex-start",
+  },
+  skipText:{
+    color: "#238A02",
+    alignItems: "flex-end",
+    marginTop: 30,
+    marginLeft: 10,
+    fontSize: 20,
+  },
+});
+
+export default ForgetPass;
